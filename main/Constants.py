eeeb57201830fbf372d326cf45f342c887de6b68
pegausbupt <shuoma86@gmail.com>
2012-05-16 11:15:15 +0800

    python

--- main/Constants.py
+++ main/Constants.py
@@ -4,10 +4,10 @@ Created on Apr 23, 2012
 @author: Sol
 '''
 #directory configuration
-BASE_DIR = 'C:/Users/v-shuoma/Desktop/workspace/data/'
+BASE_DIR = 'C:/Users/v-shuoma/Desktop/workspace/taxi/'
 RAW_DIR = BASE_DIR + 'raw/'
 PROCESSED_DIR = BASE_DIR + 'processed/'
-SAVEDIR='C:/Users/v-shuoma/Desktop/workspace/data/results/'
+SAVEDIR='C:\Users\v-shuoma\Desktop\Dropbox\experiment_results'
 
 INF = 9999999
 
